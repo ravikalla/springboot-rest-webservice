8eb6f1bf0ec65172026643b2c3be5d004a634e45
Ravi Shankar Kalla <ravi523096@yahoo.com>
2017-01-28 09:58:30 -0500

    Update SampleControllerTests.java

--- src/test/java/in/ravikalla/SampleControllerTests.java
+++ src/test/java/in/ravikalla/SampleControllerTests.java
@@ -1,5 +1,6 @@
 package in.ravikalla;
 
+import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
